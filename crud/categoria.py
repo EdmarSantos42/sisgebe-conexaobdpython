a912f6b8b07643a119f73f69ee0979a6ff2a38c8
Edmar Santos <edmarsantos42@gmail.com>
2025-08-08 19:36:16 +0000

    início do crud da tabela livro

--- crud/categoria.py
+++ crud/categoria.py
@@ -12,7 +12,6 @@ def criar_categoria(nome, descricao):
     finally:
         conn.close()
         
-
 def listar_categorias():
     try:
         conn = conectar()
@@ -40,9 +39,15 @@ def atualizar_categoria(id_categoria, novo_nome, nova_descricao):
         conn.close()
 
 def deletar_categoria(id_categoria):
-    conn = conectar()
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM Categoria WHERE id=%s", (id_categoria,))
-    conn.commit()
-    conn.close()
-    print("Categoria excluída!")
+    try:
+        conn = conectar()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM Categoria WHERE id=%s", (id_categoria,))
+        conn.commit()
+        if cursor.rowcount == 0:
+            return{"status":"aviso","mensagem":"Nenhuma categoria encontrada para deletar."}
+        return{"status":"sucesso","mensagem":"Categoria excluida!"}
+    except Exception as e:
+        return{"status":"erro","mensagem":str(e)}
+    finally:
+        conn.close()
